52fd158431d7b3851a637605556299254f849f73
87YLu <1790599287@qq.com>
2020-07-20 00:46:40 +0800

    注册登录功能

--- js/index.js
+++ js/index.js
@@ -12,6 +12,11 @@
 
 // 左边栏移动
 let startX, startWidth;
+
+const getScalableDivWidth = () => {
+  return startWidth = parseInt(window.getComputedStyle(document.querySelector(".scalable")).width, 10);
+};
+
 startWidth = localStorage.getItem("scalableWidth") || getScalableDivWidth();
 
 let scalable = document.querySelector(".scalable");
@@ -20,9 +25,6 @@ let body = document.documentElement;
 
 scalable.style.width = startWidth + "px";
 
-const getScalableDivWidth = () => {
-  return startWidth = parseInt(window.getComputedStyle(document.querySelector(".scalable")).width, 10);
-};
 
 const onDrag = (e) => {
   separator.style.cursor = "col-resize";
@@ -48,20 +50,149 @@ separator.addEventListener("mousedown", startDrag);
 
 // 登录入口
 let userBtn = document.querySelector(".user-btn");
+let userControl = document.querySelector(".user-control");
+userBtn.innerHTML = localStorage.getItem("now-user") || "未登录";
 userBtn.onclick = () => {
-  document.querySelector(".user-control").style.display = "block";
-}
+  userControl.style.display = "block";
+};
 
 // 用户管理
 document.querySelector(".control-close").onclick = () => {
-  document.querySelector(".user-control").style.display = "none";
-}
+  userControl.style.display = "none";
+};
 
 // 登陆注册框
 document.querySelector(".add-user").onclick = () => {
   document.querySelector(".logon-sign").style.display = "flex";
-}
+  userControl.style.display = "none";
+};
 
 document.querySelector(".logon-sign .close").onclick = () => {
   document.querySelector(".logon-sign").style.display = "none";
+};
+
+//注册登录
+let zhanghao = document.querySelector('.zhanghao');
+let mima = document.querySelector('.mima');
+let logoneBtn = document.querySelector(".logon");
+let signInBtn = document.querySelector(".sign-in");
+let mes = document.querySelector(".mes");
+
+const clearData = () => {
+  zhanghao.value = '';
+  mima.value = '';
+};
+
+logoneBtn.onclick = () => {
+  if (logoneBtn.innerHTML == '注册') {
+    logoneBtn.innerHTML = '返回登录';
+    document.querySelector(".logon-sign .title").innerHTML = '注册账号';
+    signInBtn.innerHTML = '注册';
+    clearData();
+  } else {
+    logoneBtn.innerHTML = '注册'
+    document.querySelector(".logon-sign .title").innerHTML = '账号登录'
+    signInBtn.innerHTML = '登录';
+    clearData();
+  }
+};
+// 设置错误提示
+const setMes = (value) => {
+  mes.style.display = "flex";
+  mes.innerHTML = value;
+  setTimeout(() => {
+    mes.style.display = "none";
+  }, 1000);
+};
+// 设置geiItem字符串
+const setLocalArr = () => {
+  if (localStorage.getItem("users") != null) {
+    let flag = (localStorage.getItem("users").indexOf("username") ==
+      localStorage.getItem("users").lastIndexOf("username"))
+    return flag ? [JSON.parse(localStorage.getItem("users"))] : [...JSON.parse(localStorage.getItem("users"))];
+  }
 }
+// 提取出已有的用户名数组
+const setNameArr = () => {
+  let nameArr = [];
+  let arr = setLocalArr();
+  for (let i = 0, length = arr.length; i < length; i++) {
+    nameArr.push(arr[i].username);
+  }
+  return nameArr;
+
+}
+// 按钮函数
+signInBtn.onclick = () => {
+  // 验证用户名
+  let reg = /^[a-zA-Z0-9_]{3,16}$/;
+  zhanghao.onblur = () => {
+    return reg.test(zhanghao.value) ? true : false;
+  };
+  // 信息不全时
+  if (zhanghao.value == "" || mima.value == "") {
+    setMes("请输入账号或密码");
+  }
+  // 用户名不正确时
+  else if (!zhanghao.onblur()) {
+    setMes("账号由3到16位数的英文字母，数字或下划线组成");
+  }
+  // 符合规范时
+  else {
+    // 登录系统
+    if (signInBtn.innerHTML == '登录') {
+      if (localStorage.getItem("users") == null) {
+        setMes("账号不存在！");
+      } else {
+        let nameArr = setNameArr();
+        let index = nameArr.indexOf(zhanghao.value);
+        if (index == -1) {
+          setMes("账号不存在！");
+        } else {
+          let arr = setLocalArr();
+          if (parseInt(mima.value, 10) == parseInt(arr[index].password, 10)) {
+            localStorage.setItem("now-user", zhanghao.value);
+            document.querySelector(".logon-sign").style.display = "none";
+            userBtn.innerHTML = zhanghao.value;
+            console.log(userBtn.innerHTML);
+          } else {
+            setMes("密码错误！");
+          }
+        }
+      }
+    }
+    // 注册账号
+    else {
+      // 系统没有账号时
+      if (localStorage.getItem("users") == null) {
+        localStorage.setItem("users", JSON.stringify({
+          username: zhanghao.value,
+          password: mima.value
+        }));
+        setMes("注册成功！正在返回登录界面…");
+        setTimeout(() => {
+          logoneBtn.onclick();
+        }, 1200);
+      } else {
+        let arr = setLocalArr();
+        let nameArr = setNameArr();
+        // 用户已经存在时
+        if (nameArr.includes(zhanghao.value)) {
+          setMes("账号已存在！");
+        }
+        // 可以正常注册账号
+        else {
+          arr.push({
+            username: `${zhanghao.value}`,
+            password: `${mima.value}`
+          });
+          localStorage.setItem("users", JSON.stringify(arr));
+          setMes("注册成功！正在返回登录界面…");
+          setTimeout(() => {
+            logoneBtn.onclick();
+          }, 1200);
+        }
+      }
+    }
+  }
+};
